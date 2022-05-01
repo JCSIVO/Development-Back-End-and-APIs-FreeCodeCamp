3421325990eef9c4ac9ecd6f62c87e09ecac3c47
JCSIVO <104387283+JCSIVO@users.noreply.github.com>
2022-05-01 23:24:04 +0200

    Add files via upload

--- 3.MongoDB and Mongoose/myApp.js
+++ 3.MongoDB and Mongoose/myApp.js
@@ -37,33 +37,99 @@ var createManyPeople = function(arrayOfPeople, done) {
 }
 
 
+//Usar model.find() para buscar en la base de datos
+var findPeopleByName = 
+  function(personName, done){
+    Person.find({name : personName}, function (err, personFound) {
+    if (err) return console.log(err);
+    done(null, personFound);
+  });
+};
+
+
+//uso del model findOne() para devolver un unico documento
+var findOneByFood = 
+  function(food, done){
+    Person.findOne({favoriteFoods : food}, function (err, personFound) {
+    if (err) return console.log(err);
+    done(null, personFound);
+  });
+};
+
+
+//uso del model findById para buscar datos por Id
+var findPersonById = function(personId, done) {
+  Person.findById(personId, function (err, data) {
+    if (err) return console.log(err);
+    done(null, data);
+  });
+};
+
+
+//Realiza las actualizaciones clásicas terminadas "find", "edit" y "save"
+
+const findEditThenSave = (personId, done) => {
+  const foodToAdd = 'hamburger';
+
+  // .findById() method to find a person by _id with the parameter personId as search key. 
+  Person.findById(personId, (err, person) => {
+    if(err) return console.log(err); 
+  
+    // Array.push() method to add "hamburger" to the list of the person's favoriteFoods
+    person.favoriteFoods.push(foodToAdd);
+
+    // and inside the find callback - save() the updated Person.
+    person.save((err, updatedPerson) => {
+      if(err) return console.log(err);
+      done(null, updatedPerson)
+    })
+  })
+};
+
+
+
+//Realiza nuevas actualizaciones en un documento usando model.findOneAndUpdate()
+
+const findAndUpdate = (personName, done) => {
+  const ageToSet = 20;
+
+  Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true}, (err, updatedDoc) => {
+    if(err) return console.log(err);
+    done(null, updatedDoc);
+  })
+};
+
+
+
+
+
 //let Person;
 
 //const createAndSavePerson = (done) => {
 //  done(null /*, data*/);
 //};
 
-const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
-};
+//const createManyPeople = (arrayOfPeople, done) => {
+//  done(null /*, data*/);
+//};
 
-const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
-};
+//const findPeopleByName = (personName, done) => {
+ // done(null /*, data*/);
+//};
 
-const findOneByFood = (food, done) => {
-  done(null /*, data*/);
-};
+//const findOneByFood = (food, done) => {
+//  done(null /*, data*/);
+//};
 
-const findPersonById = (personId, done) => {
-  done(null /*, data*/);
-};
+//const findPersonById = (personId, done) => {
+ // done(null /*, data*/);
+//};
 
-const findEditThenSave = (personId, done) => {
-  const foodToAdd = "hamburger";
+//const findEditThenSave = (personId, done) => {
+ // const foodToAdd = "hamburger";
 
-  done(null /*, data*/);
-};
+  //done(null /*, data*/);
+//};
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
